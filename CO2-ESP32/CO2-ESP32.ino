849a642a46423aa1c8f2f6a96b36c27d6010d829
machtnichts <49335465+machtnichts@users.noreply.github.com>
2020-12-26 19:28:48 +0100

    ...

---
+++ CO2-ESP32/CO2-ESP32.ino
@@ -0,0 +1,103 @@
+#include "DHTesp.h"
+#include "esp_sleep.h"
+#include "WIFI-credentials.h"
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+#define PWMPIN 25
+#define DHTPIN 23
+
+DHTesp dht;
+
+unsigned long th, tl,ppm, ppm2, ppm3, p1, p2, tpwm = 0;
+
+const uint32_t SLEEP_DURATION = 20 * 1000000; // µs
+
+const String vCO2 = "ESP32C023";
+const String vHum = "ESP32Humidity3";
+const String vTemp = "ESP32Temperature3";
+const String vLog = "ESP32Logger3"; 
+
+
+void lightSleep() {
+    esp_sleep_enable_timer_wakeup(SLEEP_DURATION);
+    delay(100);
+    esp_light_sleep_start();
+}
+
+void connectToNetwork() {
+  WiFi.begin( WIFI_NAME, WIFI_PASSWORD);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Establishing connection to WiFi..");
+  }
+ 
+  Serial.println("Connected to network");
+ 
+}
+
+int readCO2PWM()
+{ 
+  int ppm = -1;
+  do {
+    th = pulseIn(PWMPIN, HIGH, 1004000) / 1000;
+    tl = 1004 - th;
+    ppm = 5000 * (th-2)/(th+tl-4);
+  } while (th == 0);
+  return ppm;
+}
+
+void putItemValue(String itemName, String itemValue)
+{  
+  String url = "http://192.168.178.9:8080/rest/items/" + itemName + "/state";
+  HTTPClient http;
+  http.begin(url); //HTTP
+  int httpCode = http.PUT(itemValue);
+}
+
+void setup() {
+
+    Serial.begin(115200);    
+    Serial.println();
+    Serial.println();
+    Serial.println();
+
+    for(uint8_t t = 4; t > 0; t--) {
+        Serial.printf("[SETUP] WAIT %d...\n", t);
+        Serial.flush();
+        delay(1000);
+    }
+    
+    connectToNetwork();
+    
+  pinMode(PWMPIN, INPUT);
+  dht.setup(DHTPIN, DHTesp::AM2302);
+    
+}
+
+void loop() { // run over and over  
+  putItemValue(vLog,"awaken");
+  Serial.printf("reading PPM...\n");
+  Serial.flush();
+  int ppmCO2 = readCO2PWM();
+  putItemValue(vCO2,String(ppmCO2));
+
+  Serial.printf("read PPM: %d\n", ppmCO2);
+  Serial.flush();
+  
+  Serial.printf("reading temp...\n");
+  Serial.flush();
+  TempAndHumidity newValues = dht.getTempAndHumidity();
+  if (dht.getStatus() == 0) {
+    Serial.printf("temp: %f humi: %f\n",newValues.temperature, newValues.humidity);        
+    putItemValue(vHum,String(newValues.humidity));
+    putItemValue(vTemp,String(newValues.temperature));
+  } else
+    Serial.println("DHT READ FAILED...");   
+  Serial.flush();
+  
+  Serial.println("sleeping...");
+  putItemValue(vLog,"zzZZ");
+  lightSleep();
+}
---
+++ TestMH-Z19B/TestMH-Z19B.ino
@@ -0,0 +1,106 @@
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+
+/*
+ * content of WIFI-credentials.h
+ * #define WIFI_NAME "your WIFI name"
+ * #define WIFI_PASSWORD "your WIFI password"
+*/
+#include "WIFI-credentials.h"
+
+#define D0  16
+#define D1  5
+#define D2  4
+#define D3  0
+#define D4  2
+#define D5  14
+#define D6  12
+#define D7  13
+#define D8  15
+#define D9  3
+#define D10 1
+
+#define PWM1 D5
+#define PWM2 D6
+#define PWM3 D7
+#define CO2_PWR D8
+
+const int delayInSeconds = 900; //15 min
+
+ESP8266WiFiMulti WiFiMulti;
+
+void setup() {  
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP(WIFI_NAME,WIFI_PASSWORD);    
+  while(WiFiMulti.run() != WL_CONNECTED)
+    delay(10);  
+
+  pinMode(PWM1, INPUT);
+  pinMode(PWM2, INPUT);
+  pinMode(PWM3, INPUT);
+  pinMode(CO2_PWR, OUTPUT);
+  digitalWrite(CO2_PWR, LOW);
+}
+
+void loop() {  
+  log2("Starting...");
+  monitorCO2();
+  log2("Sleeping for 15 minutes...");
+  delay(delayInSeconds*1000);
+}
+
+void monitorCO2(){
+  powerOn();
+  log2("waiting 120 sec...");
+  for(uint8_t t = 24; t > 0; t--) {
+    delay(5000);
+    log2(String(t));
+  }
+  log2("reading D5...");
+  putItemValue("MM_D5",String(readCO2PWM(PWM1)));
+  log2("reading D6...");
+  putItemValue("MM_D6",String(readCO2PWM(PWM2)));
+  log2("reading D7...");
+  putItemValue("MM_D7",String(readCO2PWM(PWM3)));
+  log2("reading done.");
+  powerOff();
+}
+
+void powerOn(){
+  log1("power on");
+  digitalWrite(CO2_PWR, HIGH);
+}
+
+void powerOff(){  
+  digitalWrite(CO2_PWR, LOW);
+  log1("power off");
+}
+
+void putItemValue(String itemName, String itemValue){
+  String url = "http://192.168.178.9:8080/rest/items/" + itemName + "/state";
+  HTTPClient http;
+  http.begin(url); //HTTP
+  int httpCode = http.PUT(itemValue);
+  http.end();
+}
+
+void log1(String msg){
+  putItemValue("MM_LOG1",msg);
+}
+
+void log2(String msg){
+  putItemValue("MM_LOG2",msg);
+}
+
+int readCO2PWM(uint8_t pin)
+{ 
+  unsigned long th, tl, ppm = 0;
+  do {
+    th = pulseIn(pin, HIGH, 1004000) / 1000;
+    tl = 1004 - th;
+    ppm = 5000 * (th-2)/(th+tl-4);
+  } while (th == 0);
+  return ppm;
+}
